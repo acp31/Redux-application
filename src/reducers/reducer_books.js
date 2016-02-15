85a2e2505d4e78cab70006bdce1cf7e72d818c3e
ACP31 <aaronp48@gmail.com>
2016-02-14 23:49:00 -0800

    Starting to connect React with Redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+ export deault function() {
+  return [
+    { title: 'JavaScript: the Good Parts'},
+    { title: 'Harry Potter'},
+    { title: 'Dark Tower'},
+    { title: 'Eloquent Ruby'}
+  ]
+ }
